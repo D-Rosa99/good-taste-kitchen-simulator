83c6bc8378251ffc1e31040ba4d5dbf842371818
David De La Rosa <davidrr261096@hotmail.com>
2026-07-31 14:54:11 -0400

    Remove tableNumber property from customer entity

--- src/entity/Customer.js
+++ src/entity/Customer.js
@@ -4,19 +4,17 @@
  * @property {string} name
  * @property {string} phoneNumber
  * @property {string} address
- * @property {number} tableNumber
  */
 
 class Customer {
   /**
    * @param {CustomerProp} config
    */
-  constructor({ id, name, tableNumber, phoneNumber = null, address = null }) {
+  constructor({ id, name, phoneNumber = null, address = null }) {
     this.id = id;
     this.name = name;
     this.phoneNumber = phoneNumber;
     this.address = address;
-    this.tableNumber = tableNumber;
   }
 }
 
